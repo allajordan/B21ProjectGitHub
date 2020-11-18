992ae92fa22c37e0e19f1c240ef8013b423ddefa
Arnela <arnellamiloss@gmail.com>
2020-11-18 18:02:20 -0500

    PROJECT IS ALREADY SHARED IN REMOTE REPO

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,8 @@ public class Main {
         //B21 is good at IntelliJ
         //Mac and wondos are happy
         //everyone
+        // I have already published
+        //ready to make more coomits
+        //its to time to commit and push!
     }
 }
